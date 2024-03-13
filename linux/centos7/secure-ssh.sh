a8b367c15407c2997d2e52201af9b71c5065b992
Avery-Luther <avery.luther@mymail.champlain.edu>
2024-03-13 18:01:58 -0400

    updated script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -8,14 +8,16 @@
 
 adduser $1
 passwd $1
-git clone https://github.com/Avery-Luther/SYS-265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys/id_rsa.pub
+git clone https://github.com/Avery-Luther/SYS-265 /tmp/git
+cp /tmp/git/SYS-265/linux/public-keys/id_rsa.pub  /home/$1/.ssh/authorized_keys/id_rsa.pub
+
 
 SSHFILE=$(cat /etc/ssh/sshd_config | grep "DenyUsers")
 SSHCON="DenyUsers root"
-if [ "$SSHFILE" = "$SSHCON" ]; then
-	echo "Fail";
-else
+if [ "$SSHFILE" != "$SSHCON" ]; then
 	echo "DenyUsers root" >> /etc/ssh/sshd_config;
+else
+	echo "Already Denying Root";
 fi
 systemctl restart sshd
-
+rm -rf /tmp/git
